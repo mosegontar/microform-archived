5e9e3945540178e5351c2e008b0a1b6ecd18dd54
Alex Gontar <mosegontar@gmail.com>
2017-08-23 23:26:44 -0400

    Initial working version

---
+++ mercury.py
@@ -0,0 +1,38 @@
+"""This is a small wrapper for the Mercury Web Parser API.
+
+The code here is basically the same as Kenneth Reitz's mercury-parser
+project (https://github.com/kennethreitz/mercury-parser). It is modified
+slightly here to simply return a dict representation of the JSON response
+from Mercury Web Parser, as well as to make use of requests-cache.
+"""
+import os
+import requests
+import requests_cache
+
+
+MERCURY_DB_NAME = os.environ.get('MERCURY_DB_NAME', 'mercury')
+
+requests_cache.install_cache(
+    MERCURY_DB_NAME,
+    backend='sqlite',
+    expire_after=300#(#60*60*24*7*4) # 4 weeks
+)
+
+
+class Mercury(object):
+
+    def __init__(self, api_key):
+
+        self.api_url = 'https://mercury.postlight.com/parser?url='
+        self.api_key = api_key
+        self.requests = requests.Session()
+
+    def get(self, doc_url, cache_disabled=False):
+        headers = {'x-api-key': self.api_key}
+        url = self.api_url + doc_url
+
+        if cache_disabled:
+            with requests_cache.disabled():
+                return self.requests.get(url, headers=headers).json()
+
+        return self.requests.get(url, headers=headers).json()
---
+++ reader.py
@@ -0,0 +1,70 @@
+import html
+import os
+import pydoc
+import sys
+import tempfile
+from subprocess import call, Popen, PIPE
+
+from tomd import Tomd
+
+from mercury import Mercury
+
+
+class Reader(object):
+
+    def __init__(self, api_key):
+        self.parser = Mercury(api_key)
+        self.pager = self._create_pager_args()
+
+    def read(self, url):
+        result = self.parser.get(url)
+        if not result.get('content'):
+            print('No content')
+            return
+
+        article = self._format(result['content'])
+
+        self._display(article)
+
+    def _display(self, article):
+        if self.pager:
+            with tempfile.NamedTemporaryFile(suffix='.tmp') as tf:
+                tf.write(bytes(article, 'utf-8'))
+                tf.flush()
+                call(self.pager+[tf.name])
+
+                tf.seek(0)
+                tf.read()
+        else:
+            print(article)
+
+    def _create_pager_args(self):
+        pager = os.environ.get('PAGER')
+        if not pager:
+            return None
+        return pager.split()
+
+    def _format(self, content):
+        unescaped_html = html.unescape(content)
+        return Tomd(unescaped_html).markdown
+
+
+def main():
+    api_key = os.environ.get('MERCURY_API_KEY')
+    url = sys.argv[1]
+
+    if not api_key:
+        print('You need to set an Mercury Parser API key as an environment variable',
+              'E.g., on Bash: "export MERCURY_API_KEY=XXXXXXXXXXXXX"')
+        return
+
+    if not url:
+        print('You need to pass a url as an argument to reader',
+              'E.g., "python reader.py https://somearticle.com')
+        return
+
+    reader = Reader(api_key).read(url)
+
+
+if __name__ == '__main__':
+    main()
