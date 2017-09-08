2cb0ffe555fc903e68523215b9bdeb14faae9684
Alex Gontar <mosegontar@gmail.com>
2017-09-07 23:00:51 -0400

    update to 0.1.5

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ required = [
 
 setup(
     name='microform',
-    version='0.1.2',
+    version='0.1.5',
     description='command-line utility for reading articles in the terminal',
     long_description= 'It uses Mercury Web Parser and tomd (an HTML to Markdown converter) to fetch a web page and generate a readable, markdown version of its content. microform serves a purpose similar to other "reader" services, such as Readability, Safari Reader, Firefox Reader View, Instapaper.',
     author='Alexander Gontar',
