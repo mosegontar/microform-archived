4bd525f72c29031d4af15dd675857e66bf830dbd
Alex Gontar <mosegontar@gmail.com>
2017-09-30 12:18:41 -0400

    Add setup.py 0.1.7

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 import os
 import sys
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 if sys.argv[-1] == "publish":
     os.system("python setup.py sdist bdist_wheel upload")
@@ -18,7 +18,7 @@ required = [
 
 setup(
     name='microform',
-    version='0.1.6',
+    version='0.1.7',
     description='command-line utility for reading articles in the terminal',
     long_description= 'It uses Mercury Web Parser and tomd (an HTML to Markdown converter) to fetch a web page and generate a readable, markdown version of its content. microform serves a purpose similar to other "reader" services, such as Readability, Safari Reader, Firefox Reader View, Instapaper.',
     author='Alexander Gontar',
@@ -26,8 +26,8 @@ setup(
     url='https://github.com/mosegontar/microform',
     packages=find_packages(exclude=('tests',)),
     #py_modules=['mercury', 'microform'],
-    #entry_points= {'console_scripts': ['microform = microform:main']},
+    entry_points= {'console_scripts': ['microform = microform:microform:main']},
     install_requires=required,
-    include_package_data=True,:
+    include_package_data=True,
     license='MIT',
 )
